98fa15dfe2b000b3232f64272d8add56f3e7e282
Adolfo Aráuz <acramireza@uamv.edu.ni>
2026-09-16 16:04:57 -0600

    Update LoginFXML y arranque del login

--- EventosyNavegacion_Caso2/src/main/java/module-info.java
+++ EventosyNavegacion_Caso2/src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module ni.edu.uam.eventosynavegacion_caso2 {
+
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens ni.edu.uam.eventosynavegacion_caso2 to javafx.fxml;
+    opens ni.edu.uam.eventosynavegacion_caso2.controllers to javafx.fxml;
+
     exports ni.edu.uam.eventosynavegacion_caso2;
 }
--- EventosyNavegacion_Caso2/src/main/java/ni/edu/uam/eventosynavegacion_caso2/HelloApplication.java
+++ EventosyNavegacion_Caso2/src/main/java/ni/edu/uam/eventosynavegacion_caso2/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/ni/edu/uam/eventosynavegacion_caso2/fxml/login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Registro!");
         stage.setScene(scene);
         stage.show();
     }
